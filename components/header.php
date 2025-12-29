9ac10f51ab031793cd81dbe8686cc508683cb2a4
Edge Technosoft pvt. ltd.neha3112-edge <support@edgetechnosoft.com>
2025-12-29 16:47:58 +0530

    Update Favicon

--- components/header.php
+++ components/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="icon" type="image/x-icon" href="assets/img/des-favicon.png"/>
+    <link rel="icon" type="image/x-icon" href="assets/img/favicon.png"/>
     <title>EdgeWood University</title>
     <meta decription="Explore the best Online DBA Programs in 2025. Check fees, eligibility, and admission details for the Doctor of Business Administration course.">
     <meta keyword="Online DBA Programs">
